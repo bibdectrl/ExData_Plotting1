db7dcd9f71cf8a98585275a686fac8c7f5093424
bibdectrl <patrick@localhost.localdomain>
2014-05-06 07:15:40 -0400

    got second plot working

--- make_graphs.R
+++ make_graphs.R
@@ -1,9 +1,9 @@
 process.power.data <- function(file) {
   my.data <- read.csv(file)
-  my.data[,1] <- as.Date(my.data[,1])
+  #my.data[,1] <- as.Date(my.data[,1])
+  my.data[,1] <- as.Date(my.data[,1], "%d/%m/%Y")
   my.col.names <- c("date", "time", "global_active_power", "global_reactive_power", "voltage", "global_intensity", "sub_metering_1", "sub_metering_2", "sub_metering_3")
   my.data.frame <- data.frame(my.data)
   colnames(my.data.frame) <- my.col.names
   my.data.frame
 }
-
--- plot2.R
+++ plot2.R
@@ -1,5 +1,7 @@
 source("make_graphs.R")
 my.data <- process.power.data("pared_down_data.csv")
 png(file="plot2.png")
-plot(my.data$global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
+plot(xlab="", ylab = "Global Active Power (kilowatts)", x = as.POSIXct(paste(my.data$date, my.data$time), format="%Y-%m-%d %H:%M:%S"), y = my.data$global_active_power, type="l")
 dev.off()
+
+
